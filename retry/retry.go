1085da8078e82ed0b56106c901fd47fac2e7f98a
jinchaozhu <jinchaozhu@tencent.com>
2025-06-14 14:50:49 +0800

    fead:add retry

---
+++ retry/retry.go
@@ -0,0 +1,34 @@
+package retry
+
+import (
+	"fmt"
+	"time"
+)
+
+// 周期性的检查条件是否满足
+func Eventually(condition func() bool, waitFor time.Duration, tick time.Duration) bool {
+	ch := make(chan bool, 1)
+
+	timer := time.NewTicker(waitFor)
+	defer timer.Stop()
+
+	ticker := time.NewTicker(tick)
+	defer ticker.Stop()
+
+	for tick := ticker.C; ; {
+		select {
+		case <-timer.C:
+			fmt.Println("condition not met in time")
+			return false
+		case <-tick:
+			tick = nil
+			go func() { ch <- condition() }()
+		case v := <-ch:
+			if v {
+				return true
+			}
+			tick = ticker.C
+		}
+	}
+
+}
